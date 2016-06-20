f662958dbb0ff433e09df46367117c3a77e09109
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-06-20 02:35:11 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears values (1, 'Mr. Chocolate', 2, 'M', 'red','angry',0);
+INSERT INTO bears values (2,'Rowdy', 30, 'F', 'basdsdfs', 'ok', 1);
+INSERT INTO bears values (3,'Tabitha', 39, 'M', 'basdfsd', 'ok', 0);
+INSERT INTO bears values (4,'Sergeant Brown', 83, 'F', 'bdfgasd', 'ok', 0);
+INSERT INTO bears values (5,'Melissa', 43, 'F', 'bas23423d', 'ok', 1);
+INSERT INTO bears values (6,'Grinch', 32, 'M', 'basddfg', 'ok', 1);
+INSERT INTO bears values (7,'Wendy', 42, 'F', 'basdxcvxvc', 'ok', 1);
+INSERT INTO bears values (8,null, 123, 'M', 'basdsdasd', 'ok', 0);
